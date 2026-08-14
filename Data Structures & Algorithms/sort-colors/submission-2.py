0541c3daa7fc8cdcb1f6c30a1f99ea37217fab1f
Azamat <101340627+Azamat2007M@users.noreply.github.com>
2026-08-14 19:56:39 +0500

    Add: sort-colors - submission-2

---
+++ Data Structures & Algorithms/sort-colors/submission-2.py
@@ -0,0 +1,36 @@
+import random
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        buckets = [0, 0, 0]
+
+        for num in nums:
+            buckets[num] += 1
+
+        idx = 0
+
+        for color in range(3):
+            for _ in range(buckets[color]):
+                nums[idx] = color
+                idx += 1
+        
+        return nums
+
+        # left, mid = 0, 0
+        # right = len(nums) - 1
+
+        # while mid <= right:
+        #     if nums[mid] == 0:
+        #         nums[left], nums[mid] = nums[mid], nums[left]
+        #         mid+=1
+        #         left+=1
+        #     elif nums[mid] == 1:
+        #         mid+=1
+        #     else:
+        #         nums[mid], nums[right] = nums[right], nums[mid]
+        #         right-=1
+            
+        # return nums
+
