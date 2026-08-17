7ce3a9b1e2cb60863d041adbb7e514bf8a3d76f4
Azamat <101340627+Azamat2007M@users.noreply.github.com>
2026-08-17 15:58:52 +0500

    Add: majority-element-ii - submission-5

---
+++ Data Structures & Algorithms/majority-element-ii/submission-5.py
@@ -0,0 +1,28 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        cand1, cand2 = None, None
+        count1, count2 = 0, 0 
+
+        for num in nums:
+            if cand1 == num:
+                count1 += 1
+            elif cand2 == num:
+                count2 += 1
+            elif count1 == 0:
+                cand1 = num
+                count1 = 1
+            elif count2 == 0:
+                cand2 = num
+                count2 = 1
+            else:
+                count1 -= 1
+                count2 -= 1
+            
+        res = []
+        n = len(nums)
+
+        for cand in (cand1, cand2):
+            if cand is not None and nums.count(cand) > n // 3:
+                res.append(cand)
+
+        return res
