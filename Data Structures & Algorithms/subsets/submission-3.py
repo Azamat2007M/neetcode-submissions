c6b42b1ca38ff10047531fe506b10c373731c754
Azamat <101340627+Azamat2007M@users.noreply.github.com>
2026-09-20 16:55:47 +0500

    Add: subsets - submission-3

---
+++ Data Structures & Algorithms/subsets/submission-3.py
@@ -0,0 +1,27 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        #Backtracking method Time: O(n*2^n) Space: O(n)
+        res = []
+        subsets = []
+
+        def dfs(i):
+            if i >= len(nums):
+                res.append(subsets.copy())
+                return
+            
+            subsets.append(nums[i])
+            dfs(i + 1)
+
+            subsets.pop()
+            dfs(i + 1)
+
+        dfs(0)
+        return res
+
+        #Backtracking method Time: O(n*2^n) Space: O(n)
+        res = [[]]
+        
+        for num in nums:
+            res += [curr + num for curr in res]
+
+        return res
